d1f820c7190798033c41c4989a9c68ce7d45783a
felipeLx <felipealvesfinanceiro@gmail.com>
2019-11-28 21:41:40 -0300

    add navlink in the place of link

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
+// import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
+import './Posts.css';
 
 class Posts extends Component {
     state = {
@@ -9,6 +11,7 @@ class Posts extends Component {
     }
 
     componentDidMount () {
+        console.log(this.props);
         axios.get( '/posts' )
             .then( response => {
                 const posts = response.data.slice(0, 4);
@@ -28,18 +31,22 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        //this.props.history.push({pathname: '/' + id});
+        this.props.history.push('/' + id);
     }
 
     render() {
         let posts = <p style={{textAlign:'center'}}>Something went wrong</p>
         if(!this.state.error) {
             posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
+                return (
+                   // <Link to={'/' + post.id} key={post.id} >
+                        <Post 
+                        key={post.id}
+                        title={post.title} 
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)} />;
+                   // </Link>)
             });
         }
 
